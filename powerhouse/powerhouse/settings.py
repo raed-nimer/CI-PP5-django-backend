fac70b013f0ffbc4aebca590fbacb13f556c7cd1
Raed Nimer <raed.nimer7@gmail.com>
2025-05-04 11:04:39 +0000

    feat: Add deployed Api URL to ALLOWED_HOSTS

--- powerhouse/powerhouse/settings.py
+++ powerhouse/powerhouse/settings.py
@@ -29,12 +29,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
-    '8000-raednimer-cipp5djangoba-fmuq0cvcfjv.ws-eu118.gitpod.io'
+    '8000-raednimer-cipp5djangoba-fmuq0cvcfjv.ws-eu118.gitpod.io',
+    'django-powerhouse-api-2c217eecb4f4.herokuapp.com'
 ]
 
 
