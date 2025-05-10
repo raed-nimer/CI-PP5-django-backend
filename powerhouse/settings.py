70aaefab91baeba5a9159359f62984bb58f80d87
Raed Nimer <raed.nimer7@gmail.com>
2025-05-10 18:24:15 +0000

    fix: Turn off debug

--- powerhouse/settings.py
+++ powerhouse/settings.py
@@ -29,8 +29,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = 'DEV' in os.environ
-DEBUG=True
+DEBUG = 'DEV' in os.environ
+
 
 ALLOWED_HOSTS = [
     'localhost',
