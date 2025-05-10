025e9a746093d8fe925186aa110df71b43a51c27
Raed Nimer <raed.nimer7@gmail.com>
2025-05-10 17:55:38 +0000

    feat: Use cloudinary for product image upload

--- powerhouse/product/models.py
+++ powerhouse/product/models.py
@@ -17,7 +17,7 @@ class Product(models.Model):
     description = models.TextField()  # required
     price = models.DecimalField(max_digits=10, decimal_places=2)  # required
     category = models.ForeignKey(Category, on_delete=models.CASCADE)  # required
-    image = CloudinaryField('image', default='products/default.jpg')
+    image = CloudinaryField('image')
     #image = models.ImageField(upload_to='products/', blank=True, null=True, default='products/default.jpg')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
