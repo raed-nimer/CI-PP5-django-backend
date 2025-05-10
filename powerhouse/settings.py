2b1ac15ceb7a8745253bcb07330c701e2fd54abf
Raed Nimer <raed.nimer7@gmail.com>
2025-05-10 18:22:16 +0000

    fix: Turn on debugger to fix 400 error

--- powerhouse/settings.py
+++ powerhouse/settings.py
@@ -29,14 +29,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEV' in os.environ
+#DEBUG = 'DEV' in os.environ
+DEBUG=True
 
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
     'http://localhost:5173',
     '8000-raednimer-cipp5djangoba-fmuq0cvcfjv.ws-eu118.gitpod.io',
-    'https://django-powerhouse-api-2c217eecb4f4.herokuapp.com'
+    'https://django-powerhouse-api-2c217eecb4f4.herokuapp.com',
+    '.herokuapp.com'
 ]
 
 CSRF_TRUSTED_ORIGINS = [
