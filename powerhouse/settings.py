a31303b93777da894efa858e2c6808a1ac8d3138
Raed Nimer <raed.nimer7@gmail.com>
2025-05-10 18:18:12 +0000

    fix: Remove slash from ALLOWED_HOST url

--- powerhouse/settings.py
+++ powerhouse/settings.py
@@ -36,7 +36,7 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
     'http://localhost:5173',
     '8000-raednimer-cipp5djangoba-fmuq0cvcfjv.ws-eu118.gitpod.io',
-    'https://django-powerhouse-api-2c217eecb4f4.herokuapp.com/'
+    'https://django-powerhouse-api-2c217eecb4f4.herokuapp.com'
 ]
 
 CSRF_TRUSTED_ORIGINS = [
