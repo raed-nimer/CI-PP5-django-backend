b477d6c4022d86eb102b02a83e3003fd2c22c922
Raed Nimer <raed.nimer7@gmail.com>
2025-05-21 18:51:50 +0000

    fix: Add missing imports to Cart models.py

--- cart/models.py
+++ cart/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from products.models import Product
 # Create your models here.
 class CartItem(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="cart_items")
