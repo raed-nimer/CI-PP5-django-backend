f72e2ca9c981a7219c9539876403e0269118a635
Raed Nimer <raed.nimer7@gmail.com>
2025-05-21 18:46:46 +0000

    fix: Add /contact endpoint for contact form

--- base/urls.py
+++ base/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('/contact', views.ContactFormResponseView.as_view(), name="contact")
 ]
 
 # urlpatterns += static(
