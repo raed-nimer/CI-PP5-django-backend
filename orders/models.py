348a43f5599173d0c646ceea4305bf872c952f5b
Raed Nimer <raed.nimer7@gmail.com>
2025-05-21 18:54:39 +0000

    fix: Improvements to Product model import

--- orders/models.py
+++ orders/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 from django.db import models
 from django.contrib.auth.models import User
-from products.models import Product
+from product.models import Product
 
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
