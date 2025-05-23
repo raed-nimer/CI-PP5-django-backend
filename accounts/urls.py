19536f360c82b7a59d1f2c75f42122851232af64
Raed Nimer <141240830+raed-nimer@users.noreply.github.com>
2025-05-23 14:15:16 +0500

    feat: Add profile route to urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from django.http import HttpResponse
-from .views import RegisterView, LoginView
+from .views import RegisterView, LoginView, ProfileView
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
 path('register', RegisterView.as_view(), name='signup'),
 path('login', LoginView.as_view(), name='login'),
+ path('profile', ProfileView.as_view(), name='profile'),  # GET and POST
 ]
 
 # urlpatterns += static(
