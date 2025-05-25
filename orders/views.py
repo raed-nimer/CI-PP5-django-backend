d38c5838d01385dec1cf044e9e7f8d9dfe023225
Raed Nimer <141240830+raed-nimer@users.noreply.github.com>
2025-05-25 17:29:36 +0500

    fix: Update PlaceOrderView

--- orders/views.py
+++ orders/views.py
@@ -1,31 +1,35 @@
 from .models import Order, OrderItem 
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import status, permissions
 from cart.models import CartItem
 from django.db import transaction
 # Create your views here.
 
-@api_view(['POST'])
-@permission_classes([permissions.IsAuthenticated])
-def place_order(request):
-    cart_items = CartItem.objects.filter(user=request.user)
+class PlaceOrderView(APIView):
+    permission_classes = [IsAuthenticated]
 
-    if not cart_items.exists():
-        return Response({'error': 'Your cart is empty.'}, status=status.HTTP_400_BAD_REQUEST)
+    def post(self, request):
+        cart_items = CartItem.objects.select_related('product').filter(user=request.user)
 
-    with transaction.atomic():
-        total_price = sum(item.product.price * item.quantity for item in cart_items)
-        order = Order.objects.create(user=request.user, total_price=total_price)
+        if not cart_items.exists():
+            return Response({'error': 'Your cart is empty.'}, status=status.HTTP_400_BAD_REQUEST)
 
-        for item in cart_items:
-            OrderItem.objects.create(
-                order=order,
-                product=item.product,
-                quantity=item.quantity,
-                price_at_purchase=item.product.price
-            )
+        with transaction.atomic():
+            total_price = sum(item.product.price * item.quantity for item in cart_items)
+            order = Order.objects.create(user=request.user, total_price=total_price)
+            order_items = [
+                OrderItem(
+                    order=order,
+                    product=item.product,
+                    quantity=item.quantity,
+                    price_at_purchase=item.product.price
+                ) for item in cart_items
+            ]
+            OrderItem.objects.bulk_create(order_items)
 
-        cart_items.delete()
+            cart_items.delete()
 
-    return Response({'message': 'Order placed successfully.', 'order_id': order.id}, status=status.HTTP_201_CREATED)
+        return Response({'message': 'Order placed successfully.', 'order_id': order.id}, status=status.HTTP_201_CREATED)
