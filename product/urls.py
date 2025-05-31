a90eef298f9c98f43165151b56eb583ff09b6b80
Raed Nimer <141240830+raed-nimer@users.noreply.github.com>
2025-05-31 13:16:51 +0500

    fix: Fix lint errors in product urls.py

--- product/urls.py
+++ product/urls.py
@@ -5,10 +5,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.ProductListView.as_view(), name="products"), # /products
-    path('categories/', views.CategoryListView.as_view(), name="categories"), # /products/categories
-    path('<int:pk>/', views.ProductDetailView.as_view(), name="product-detail"), # /products/1
+    path('', views.ProductListView.as_view(), name="products"),  # /products
+    path('categories/', views.CategoryListView.as_view(), name="categories"),
+    path('<int:pk>/',
+         views.ProductDetailView.as_view(), name="product-detail"),
 ]
-
-# urlpatterns += static(
-#         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
