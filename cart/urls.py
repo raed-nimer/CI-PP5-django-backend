e6e31bf22f0ffa8184176119ceafe5012ffed60d
Raed Nimer <141240830+raed-nimer@users.noreply.github.com>
2025-05-31 12:38:11 +0500

    fix: Fix lint errors in cart urls.py

--- cart/urls.py
+++ cart/urls.py
@@ -1,9 +1,30 @@
 from django.urls import path
-from .views import CartItemsView, AddToCartView, UpdateCartItemView, DeleteCartItemView
+from .views import (
+    CartItemsView,
+    AddToCartView,
+    UpdateCartItemView,
+    DeleteCartItemView
+)
 
 urlpatterns = [
-    path('', CartItemsView.as_view(), name='get_cart_items'),                  # GET all
-    path('add/', AddToCartView.as_view(), name='add_to_cart'),                 # POST
-    path('update/<int:pk>/', UpdateCartItemView.as_view(), name='update_cart_item'),  # PUT
-    path('delete/<int:pk>/', DeleteCartItemView.as_view(), name='delete_cart_item'),  # DELETE
+    path(
+        '',
+        CartItemsView.as_view(),
+        name='get_cart_items'
+    ),
+    path(
+        'add/',
+        AddToCartView.as_view(),
+        name='add_to_cart'
+    ),
+    path(
+        'update/<int:pk>/',
+        UpdateCartItemView.as_view(),
+        name='update_cart_item'
+    ),
+    path(
+        'delete/<int:pk>/',
+        DeleteCartItemView.as_view(),
+        name='delete_cart_item'
+    ),
 ]
