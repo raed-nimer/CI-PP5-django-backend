5d99d17481dae08d487b728799d74780e7632922
Raed Nimer <141240830+raed-nimer@users.noreply.github.com>
2025-05-31 12:24:25 +0500

    fix: Improvements to accounts urls.py validation

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,10 +5,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-path('register', RegisterView.as_view(), name='signup'),
-path('login', LoginView.as_view(), name='login'),
- path('profile', ProfileView.as_view(), name='profile'),  # GET and POST
+    path('register', RegisterView.as_view(), name='signup'),
+    path('login', LoginView.as_view(), name='login'),
+    path('profile', ProfileView.as_view(), name='profile'),  # GET and POST
 ]
-
-# urlpatterns += static(
-#         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
