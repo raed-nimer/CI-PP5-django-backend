dbdef57b4479b2f6b5ef3b0eec34f931ddc12e8c
Raed Nimer <141240830+raed-nimer@users.noreply.github.com>
2025-05-31 12:37:13 +0500

    fix: Fix lint errors in cart models.py

--- cart/models.py
+++ cart/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 from product.models import Product
-# Create your models here.
+
+
 class CartItem(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="cart_items")
+    user = models.ForeignKey(User,
+                             on_delete=models.CASCADE,
+                             related_name="cart_items")
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
     added_at = models.DateTimeField(auto_now_add=True)
