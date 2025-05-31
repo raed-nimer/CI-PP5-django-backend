504665d71c7b2bc406bfd889c4df6d4a19e3cc23
Raed Nimer <141240830+raed-nimer@users.noreply.github.com>
2025-05-31 12:29:27 +0500

    fix: Fix lint errors in base models.py

--- base/models.py
+++ base/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
 class ContactFormResponse(models.Model):
     name = models.CharField(max_length=100)  # string - required
     email = models.CharField(max_length=100)  # string - required
