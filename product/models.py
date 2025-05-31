1a07e2e66319a95f75d349ce14fb6c241ef2acd3
Raed Nimer <141240830+raed-nimer@users.noreply.github.com>
2025-05-31 13:00:56 +0500

    fix: Fix Lint errors in product models.py

--- product/models.py
+++ product/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from cloudinary.models import CloudinaryField
 # Create your models here.
 
+
 # Category model
 class Category(models.Model):
     name = models.CharField(max_length=200)  # required
@@ -10,15 +11,15 @@ class Category(models.Model):
 
     def __str__(self):
         return str(self.name)
-    
+
+
 # Product model
 class Product(models.Model):
     name = models.CharField(max_length=200)  # required
     description = models.TextField()  # required
     price = models.DecimalField(max_digits=10, decimal_places=2)  # required
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)  # required
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     image = CloudinaryField('image')
-    #image = models.ImageField(upload_to='products/', blank=True, null=True, default='products/default.jpg')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
