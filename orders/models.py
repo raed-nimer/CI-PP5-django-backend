6aeef10de88b12ccf5fa53f09b2a930701c3beca
Raed Nimer <141240830+raed-nimer@users.noreply.github.com>
2025-05-31 12:49:10 +0500

    fix: Fix lint errors in orders models.py

--- orders/models.py
+++ orders/models.py
@@ -5,6 +5,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from product.models import Product
 
+
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
@@ -18,11 +19,19 @@ class Order(models.Model):
     payment_method = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
 
+
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='order_items')
+    order = models.ForeignKey(
+        Order,
+        on_delete=models.CASCADE,
+        related_name='order_items'
+        )
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
-    price_at_purchase = models.DecimalField(max_digits=10, decimal_places=2)  # Save price at the time of order
+    price_at_purchase = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+        )  # Save price at the time of order
 
     def __str__(self):
         return f"{self.product.name} x{self.quantity}"
